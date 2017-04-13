852e09b732efcf2456a632aebaf78f379465a7f0
Xiomara Figueroa <figueroaxiomara7@gmail.com>
2017-04-12 23:33:00 -0400

    Started campaign collection in mongodb + publish/subscribe to collection

--- server/main.js
+++ server/main.js
@@ -1,5 +1,25 @@
 import { Meteor } from 'meteor/meteor';
+import { Campaigns } from '../imports/collections/campaigns'; 
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	const numberRecords = Campaigns.find({}).count();
+	if (!numberRecords) {
+	
+		// Dummy Data
+		Campaigns.insert({
+			title: "15 K",
+			goal: 25000,
+			status: 5000,
+			categories: ["salud"],
+			imageURL: "",
+			about: "Carrera de 15K a beneficio de los niños del Hospital X",
+			type: "active",
+			createdDate: "2017-04-08T09:02:21",
+			endDate: "2017-05-08T09:07:20"
+		}); 
+	}
+
+	Meteor.publish('campaigns', function() {
+		return Campaigns.find({}, { limit: 20 }); 
+	}); 
 });
