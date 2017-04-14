5da73d6836908c79e8da90f705c0b7b0814a2dba
Xiomara Figueroa <figueroaxiomara7@gmail.com>
2017-04-13 22:33:06 -0400

    heroku

--- admin/client/main.js
+++ admin/client/main.js
@@ -1,12 +1,1 @@
-import React from 'react'; 
-import ReactDOM from 'react-dom';
 
-import CampaignList from './components/campaign_list'; 
-
-const App = () => {
-	return (
-		<div>
-			<CampaignList />
-		</div>
-	)
-}
