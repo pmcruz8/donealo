85547d8c388a12b1fe04068502de8185a78df1cb
Xiomara Figueroa <figueroaxiomara7@gmail.com>
2017-04-14 11:23:52 -0400

    Deploying donealo to heroku

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 echo "Deploying donealo to heroku"
-cd donealo/admin
+cd admin
 npm install 
 cd ../home
 npm install 
+cd ..
 git add . 
 git commit -m "Deploying donealo to heroku"
 git push heroku master
