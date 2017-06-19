966d6ad90df63d6c61876597212baacf1eda96e4
Pedro Cruz <pedro@rokailabs.com>
2017-06-18 20:49:52 -0400

    Initial recursos y tiempo form implementation

--- client/components/profile_page/sidebar_profile.js
+++ client/components/profile_page/sidebar_profile.js
@@ -1,40 +1,70 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Modal, Button} from 'react-bootstrap';
 
 // import Navbar from './navbar';
 // import Sidebar from './sidebar_profile';
 
-const SidebarProfile = () => {
-  return (
-    <div className="profileSideBar">
+class SidebarProfile extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      showModalRecursos:false,
+      showModalTiempo: false
+    }
+
+    this.closeRecursos = this.closeRecursos.bind(this);
+    this.openRecursos = this.openRecursos.bind(this);
+    this.closeTiempo = this.closeTiempo.bind(this);
+    this.openTiempo = this.openTiempo.bind(this);
+
+  }
+
+  getInitialState() {
+    return {showModalRecursos: false,showModalTiempo:false};
+  }
+
+  closeRecursos() {
+    this.setState({showModalRecursos: false});
+  }
+
+  openRecursos() {
+    this.setState({showModalRecursos: true});
+  }
+
+  closeTiempo() {
+    this.setState({showModalTiempo: false});
+  }
+
+  openTiempo() {
+    this.setState({showModalTiempo: true});
+  }
+
+  render() {
+    return (
+      <div className="profileSideBar">
         <center>
           <h4 className="sidebarText">Escoge el tipo de donación</h4>
           <div className="row">
             <div className="col-xs-4">
               <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-               <input type="hidden" name="business" value="donations@kcparkfriends.org"/>
+                <input type="hidden" name="business" value="donations@kcparkfriends.org"/>
                 <input type="hidden" name="cmd" value="_donations"/>
                 <input type="hidden" name="item_name" value="Dona por una causa"/>
                 <input type="hidden" name="item_number" value="Donación"/>
                 <input type="hidden" name="currency_code" value="USD"/>
-                <input type="image" name="submit"
-                  data-toggle="modal" 
-                  data-target="#donateModal" 
-                  className="btn donateIcons"
-                  src="img/money-icon.png"
-                  alt="Donate"/>
+                <input type="image" name="submit" data-toggle="modal" data-target="#donateModal" className="btn donateIcons" src="img/money-icon.png" alt="Donate"/>
                 <h5>Dinero</h5>
               </form>
-              </div>
-            
-
-            <div className="col-xs-4">
-              <img type="button" data-toggle="modal" data-target="#giveModal" className="btn donateIcons" src="img/give-icon.png" alt=""/>
-              <h5>Artículos</h5>
             </div>
 
             <div className="col-xs-4">
-              <img type="button" data-toggle="modal" data-target="#timeModal" className="btn donateIcons" src="img/time-icon.png" alt=""/>
+              <img type="button" className="btn donateIcons" src="img/give-icon.png" alt="" onClick={this.openRecursos}/>
+              <h5>Recursos</h5>
+            </div>
+
+            <div className="col-xs-4">
+              <img type="button" data-toggle="modal" data-target="#timeModal" className="btn donateIcons" src="img/time-icon.png" alt="" onClick={this.openTiempo}/>
               <h5>Tiempo</h5>
             </div>
 
@@ -56,9 +86,83 @@ const SidebarProfile = () => {
             </div>
           </div>
 
-      </center>
+        </center>
+
+        <Modal show={this.state.showModalRecursos} onHide={this.closeRecursos}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <h3 className="text-center">Dona Recursos</h3>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <form action="form">
+                <div className="col-md-12 m30">
+                  <label>Artículos</label><br />
+                  <textarea className="input100" name="articles" rows="5"></textarea>
+                </div>
+                <div className="col-md-6 m30">
+                  <label>First Name</label><br />
+                  <input className="input100"  type="text" name="firstname" />
+                </div>
+                <div className="col-md-6 m30">
+                  <label>Last Name</label><br />
+                  <input className="input100"  type="text" name="lastname" />
+                </div>
+                <div className="col-md-6 m30">
+                  <label>Telephone</label><br />
+                  <input className="input100"  type="text" name="telephone" />
+                </div>
+                <div className="col-md-6 m30">
+                  <label>Email</label><br />
+                  <input className="input100"  type="text" name="email" />
+                </div>
+                <input type="submit" value="Submit" />
+              </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeRecursos}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Modal show={this.state.showModalTiempo} onHide={this.closeTiempo}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <h3 className="text-center">Dona Tiempo</h3>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <form action="form">
+                <div className="col-md-12 m30">
+                  <label>Horario Disponible</label><br />
+                  <input className="input100" name="horario"></input>
+                </div>
+                <div className="col-md-6 m30">
+                  <label>First Name</label><br />
+                  <input className="input100"  type="text" name="firstname" />
+                </div>
+                <div className="col-md-6 m30">
+                  <label>Last Name</label><br />
+                  <input className="input100"  type="text" name="lastname" />
+                </div>
+                <div className="col-md-6 m30">
+                  <label>Telephone</label><br />
+                  <input className="input100"  type="text" name="telephone" />
+                </div>
+                <div className="col-md-6 m30">
+                  <label>Email</label><br />
+                  <input className="input100"  type="text" name="email" />
+                </div>
+                <input type="submit" value="Submit" />
+              </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeTiempo}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+
       </div>
-  );
+    )
+  };
 };
 
 export default SidebarProfile;
